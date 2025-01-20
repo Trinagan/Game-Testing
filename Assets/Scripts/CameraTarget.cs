c1ce26cd358b43f7039d16b98ac3e34999d93e7d
vesper-arch <vesper-arch@proton.me>
2025-01-20 15:06:48 -0500

    Changed parameter name for clarity

--- Assets/Scripts/CameraTarget.cs
+++ Assets/Scripts/CameraTarget.cs
@@ -4,7 +4,7 @@ public class CameraTarget : MonoBehaviour
 {
     public Transform player;
     public Camera mainCamera;
-    public float cameraMagnitude = 0.2f;
+    public float cameraShiftScalar = 0.2f;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -17,7 +17,7 @@ public class CameraTarget : MonoBehaviour
         Vector2 playerPos = new Vector2(player.position.x, player.position.y);
         Vector2 mousePos = mainCamera.ScreenToWorldPoint(Input.mousePosition);
 
-        Vector2 cameraPos = playerPos + (mousePos - playerPos) * cameraMagnitude;
+        Vector2 cameraPos = playerPos + (mousePos - playerPos) * cameraShiftScalar;
         
         transform.position = new Vector3(cameraPos.x, cameraPos.y, -10);
     }
