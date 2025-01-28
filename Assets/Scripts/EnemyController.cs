cafcfd13508d40be32f0538007fd0a65505768c9
Richard Goodwins <pixotic@pixotic.org>
2025-01-28 20:03:46 +0000

    Enemy health prep foundation

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -6,7 +6,20 @@ public class EnemyController : MonoBehaviour
     [SerializeField] Transform target;
     NavMeshAgent agent;
     private Rigidbody2D rig;
-    public float health = 100;
+
+    // Implementing a base stat (which can be altered by factors) -> Max stat -> current stat system
+    // this way we're set up for possible things like healing (but capping at max hp) etc
+    public float BaseMaxHealth = 100f;
+    public float MaxHealth;
+    public float CurrentHealth;
+    
+    // Get this stuff done ASAP on load, probably would be fine in Start but I'm paranoid
+    void Awake()
+    {
+        // Init stats based on other factors
+        InitialiseStats();
+    }
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -21,9 +34,9 @@ public class EnemyController : MonoBehaviour
     {
         agent.SetDestination(target.position);
 
-        if (health <= 0)
+        if (CurrentHealth <= 0)
         {
-            Destroy(gameObject);
+            OnDeath();
         }
     }
 
@@ -31,7 +44,30 @@ public class EnemyController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Bullet"))
         {
-            health -= 30;
+            // We will want to replace this static amount with a reference to the attacking projectile's stats
+            // e.g. Bullet might do x damage, FlameThrowerTick might be 15, RPG = 50 etc
+            CurrentHealth -= 30;
         }
+    }
+
+    void InitialiseStats()
+    {
+        // Unused stub for now.
+        // Possible implementation:
+        // Scale CurrentHealth based on additional factors such as level, game difficulty or enemy variant e.g.
+        // Enemy hp scales 10% (0.1) per level, +/- 30% (0.3) based on easy/normal/hard difficulty and MeanVariant adds 25% (0.25)
+        // A MeanVariant on level 3 on the easy difficulty would be
+        // MaxHealth = BaseMaxHealth * (1 + ([LevelModifier] + [VariantModifier]) * (1 + [DifficultyModifier]))
+        // MaxHealth = 100 * 1.55 * 0.7 = 108.5
+
+        MaxHealth = BaseMaxHealth;
+        CurrentHealth = MaxHealth;
+    }
+
+    // Moved this into it's own function to give us room to handle everything that should happen in a neat way
+    // e.g. trigger animations, spawn dropped loot, trigger events etc
+    void OnDeath()
+    {
+        Destroy(gameObject);
     }
 }
